288f9b170c739bb21f97e7fc707fcc697c3412a6
alihassan-coder <alibinsalman786@gmail.com>
2025-06-04 14:44:41 +0500

    remove error

--- utils/jwt_handler.py
+++ utils/jwt_handler.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM =os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
